1e36762ca167f0db077201ccad37e51fbc8d8c2d
anicjeff <anicjeff@gmail.com>
2018-08-24 12:34:46 -0400

    push two

--- src/com/njeffery/part2.java
+++ src/com/njeffery/part2.java
@@ -1,4 +1,6 @@
 //https://www.youtube.com/watch?v=yYN8u90MKCg&list=PLE7E8B7F4856C9B19&index=2
+//https://help.github.com/articles/adding-a-file-to-a-repository-using-the-command-line/
+//https://help.github.com/articles/cloning-a-repository/
 package com.njeffery;
 
 public class part2 {
