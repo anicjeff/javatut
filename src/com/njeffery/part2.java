ed981d649fd48fd65f0281bf3f5d19528c5dd803
anicjeff <anicjeff@gmail.com>
2018-08-24 13:54:17 -0400

    push three

--- src/com/njeffery/part2.java
+++ src/com/njeffery/part2.java
@@ -2,6 +2,36 @@
 //https://help.github.com/articles/adding-a-file-to-a-repository-using-the-command-line/
 //https://help.github.com/articles/cloning-a-repository/
 package com.njeffery;
+//import package needed for user input called scanner you can use * to import entire packages
+import java.util.Scanner;
 
 public class part2 {
+    //Creates a new scanner object named userInput this is what accepts user input
+    static Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        System.out.print("your fav number: ");
+//if user input is an integer perform all of the code in statement
+//.hasNextDouble() .hasNextFloat() .hasNextShort() etc
+        //why does this work? the if statment itself is checking userinput
+        //the if statment prevents crashing if the user inputs something other than int
+        if (userInput.hasNextInt()) {
+            //recieves user input and stores in variable we expect an interger
+            //.nextFloat() .nextDouble() etc.
+            int numberEntered = userInput.nextInt();
+            System.out.println("you entered: " + numberEntered);
+            System.out.print("enter a second nnumber: ");
+            int numberEntered2 = userInput.nextInt();
+            int result = numberEntered2 + numberEntered;
+            System.out.println(result);
+        }
+    }
+
+
+
+
+
+
+
 }
